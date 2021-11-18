ce1e543614453650d3d47fa47de4ad0582d1408d
XxMoonsoulxX <77117705+XxMoonsoulxX@users.noreply.github.com>
2021-11-18 20:41:51 +0200

    Part 5/5
    Kind of complete no text on Main menu boxes and apple's not going away after playing. + there's no winning condition.

--- Games4Kids/Assets/Script/Calculator.cs
+++ Games4Kids/Assets/Script/Calculator.cs
@@ -5,18 +5,24 @@ using UnityEngine.UI;
 
 public class Calculator : MonoBehaviour
 {
-    int primeValue, secondValue, tempValue, finalAnswer, Alternative1, Alternative2;
+    int primeValue, secondValue, tempValue, finalAnswer, Alternative1, Alternative2, points;
     public Animator AnimatorBack;
-    public Text PrimeDigit, SecondDigit, Sign, Alt_1, Alt_2, Alt_3;
-    public Texture TextureSum, TextureReduce, TextureDivide, TextureMultiply;
+    public Text PrimeDigit, SecondDigit, Sign, Alt_1, Alt_2, Alt_3, Answer_Place;
+    public Sprite SpriteYes, SpriteNo, SpriteTransparent;
+    public Texture TextureSum, TextureReduce, TextureDivide, TextureMultiply;    
     private string TempoOp, VarSign;
-    public GameObject Canvas;
+    public GameObject Canvas, Right_Answer_1, Right_Answer_2, Right_Answer_3;
+    public Transform Apple;
+
+    public AudioSource AudioComponent;
+    public AudioClip SoundYes, SoundNo;
 
     // Start is called before the first frame update
     void Start()
     {
         Canvas.SetActive(false);
         GameObject.Find("Main Camera").transform.position = new Vector3(0f, 0f, -10f);
+        points = 0;
     }
 
     // Update is called once per frame
@@ -95,6 +101,8 @@ public class Calculator : MonoBehaviour
 
     public void CalcularFn(string operation)
     {
+        ResetValues();
+
         primeValue = Random.Range(1,10);
         secondValue = Random.Range(1, 10);
 
@@ -120,7 +128,7 @@ public class Calculator : MonoBehaviour
         if (operation == "divide")
         {
             finalAnswer = primeValue / secondValue;
-            Debug.Log("Division: " + finalAnswer);
+            //Debug.Log("Division: " + finalAnswer);
             VarSign = "divide";
         }
 
@@ -130,7 +138,7 @@ public class Calculator : MonoBehaviour
             VarSign = "multiply";
         }
 
-        Debug.Log("1Value: " + primeValue + " 2value: " + secondValue + "Answer is : " + finalAnswer); // For pressing Letter S,R,M and D
+        //Debug.Log("1Value: " + primeValue + " 2value: " + secondValue + "Answer is : " + finalAnswer); For pressing Letter S,R,M and D
 
         PrimeDigit.text = primeValue.ToString();
         SecondDigit.text = secondValue.ToString();
@@ -171,45 +179,120 @@ public class Calculator : MonoBehaviour
         }
         Alternative2 = tempValue;
 
-        Debug.Log("Alternatives: " + Alternative1 + " - " + Alternative2 + " - " + finalAnswer);
+        //Debug.Log("Alternatives: " + Alternative1 + " - " + Alternative2 + " - " + finalAnswer);
 
 
         tempValue = Random.Range(1, 7);
         if (tempValue == 1)
         {
             Alt_1.text = finalAnswer.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = Alternative2.ToString();
-            Debug.Log("tempValue1");
+            //Debug.Log("tempValue1");
         }
         if (tempValue == 2)
         {
             Alt_1.text = finalAnswer.ToString(); Alt_2.text = Alternative2.ToString(); Alt_3.text = Alternative1.ToString();
-            Debug.Log("tempValue2");
+            //Debug.Log("tempValue2");
         }
         if (tempValue == 3)
         {
             Alt_1.text = Alternative1.ToString(); Alt_2.text = finalAnswer.ToString(); Alt_3.text = Alternative2.ToString();
-            Debug.Log("tempValue3");
+            //Debug.Log("tempValue3");
         }
         if (tempValue == 4)
         {
             Alt_1.text = Alternative1.ToString(); Alt_2.text = Alternative2.ToString(); Alt_3.text = finalAnswer.ToString();
-            Debug.Log("tempValue4");
+            //Debug.Log("tempValue4");
         }
         if (tempValue == 5)
         {
             Alt_1.text = Alternative2.ToString(); Alt_2.text = finalAnswer.ToString(); Alt_3.text = Alternative1.ToString();
-            Debug.Log("tempValue5");
+            //Debug.Log("tempValue5");
         }
         if (tempValue == 6)
         {
             Alt_1.text = Alternative2.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = finalAnswer.ToString();
-            Debug.Log("tempValue6");
+            //Debug.Log("tempValue6");
         }
         if (tempValue == 7)
         {
             Alt_1.text = Alternative2.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = finalAnswer.ToString();
-            Debug.Log("tempValue7");
+            //Debug.Log("tempValue7");
         }
+    }
 
+    public void Alt_1_action()
+    {
+        if ( Alt_1.text == finalAnswer.ToString())
+        {
+            Right_Answer_1.GetComponent<Image>().sprite = SpriteYes;
+            GotItRight();
+        }
+        else
+        {
+            Right_Answer_1.GetComponent<Image>().sprite = SpriteNo;
+            GotItWrong();
+        }
+    }
+
+    public void Alt_2_action()
+    {
+        if (Alt_2.text == finalAnswer.ToString())
+        {
+            Right_Answer_2.GetComponent<Image>().sprite = SpriteYes;
+            GotItRight();
+        }
+        else
+        {
+            Right_Answer_2.GetComponent<Image>().sprite = SpriteNo;
+            GotItWrong();
+        }
+    }
+
+    public void Alt_3_action()
+    {
+        if (Alt_3.text == finalAnswer.ToString())
+        {
+            Right_Answer_3.GetComponent<Image>().sprite = SpriteYes;
+            GotItRight();
+        }
+        else
+        {
+            Right_Answer_3.GetComponent<Image>().sprite = SpriteNo;
+            GotItWrong();
+        }
+    }
+
+    public void ResetValues()
+    {
+        Right_Answer_1.GetComponent<Image>().sprite = SpriteTransparent;
+        Right_Answer_2.GetComponent<Image>().sprite = SpriteTransparent;
+        Right_Answer_3.GetComponent<Image>().sprite = SpriteTransparent;
+
+        Answer_Place.text = "?";        
+    }
+    
+    public void GotItWrong()
+    {
+        AudioComponent.clip = SoundNo;
+        AudioComponent.Play();
+    }
+
+    public void GotItRight()
+    {
+        Answer_Place.text = finalAnswer.ToString();
+        points = points + 1;
+
+        Instantiate(Apple , new Vector3( 12f+ (points-1)*1f, 4.3f, -1f ) , Quaternion.identity);
+
+        AudioComponent.clip = SoundYes;
+        AudioComponent.Play();
+
+        StartCoroutine(NextQuestion());
+    }
+
+    IEnumerator NextQuestion()
+    {
+        yield return new WaitForSeconds(2);
+        CalcularFn(VarSign);        
     }
 }
