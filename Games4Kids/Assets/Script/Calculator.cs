99f94b1fa40e2123fb4c282290e9b50531afa2dd
XxMoonsoulxX <77117705+XxMoonsoulxX@users.noreply.github.com>
2021-11-18 14:50:18 +0200

    Part 4/5
    Added Numbers and randomize where answer go.

--- Games4Kids/Assets/Script/Calculator.cs
+++ Games4Kids/Assets/Script/Calculator.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Calculator : MonoBehaviour
 {
-    int primeValue, secondValue, tempValue, finalAnswer;
+    int primeValue, secondValue, tempValue, finalAnswer, Alternative1, Alternative2;
     public Animator AnimatorBack;
+    public Text PrimeDigit, SecondDigit, Sign, Alt_1, Alt_2, Alt_3;
     public Texture TextureSum, TextureReduce, TextureDivide, TextureMultiply;
-    private string TempoOp;
+    private string TempoOp, VarSign;
     public GameObject Canvas;
 
     // Start is called before the first frame update
     void Start()
     {
         Canvas.SetActive(false);
+        GameObject.Find("Main Camera").transform.position = new Vector3(0f, 0f, -10f);
     }
 
     // Update is called once per frame
@@ -74,12 +77,20 @@ public class Calculator : MonoBehaviour
     {
         GameObject.Find("Main Camera").transform.position = new Vector3(20f, 0f, -10f);
 
-        if (TempoOp == "sums") {        GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureSum; }
-        if (TempoOp == "reduces") {     GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureReduce; }
-        if (TempoOp == "divides") {     GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureDivide; }
-        if (TempoOp == "multiples") {   GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureMultiply; }
+        if (TempoOp == "sums") { CalcularFn("sum");            GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureSum; }
+        if (TempoOp == "reduces") { CalcularFn("reduce");      GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureReduce; }
+        if (TempoOp == "divides") { CalcularFn("divide");      GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureDivide; }
+        if (TempoOp == "multiples") { CalcularFn("multiply");  GameObject.Find("BackgroundAfter").GetComponent<Renderer>().material.mainTexture = TextureMultiply; }
 
         Canvas.SetActive(true);
+    }
+
+    public void ReturnMenu()
+    {
+        Canvas.SetActive(false);
+        GameObject.Find("Main Camera").transform.position = new Vector3(0f, 0f, -10f);
+        AnimatorBack = GameObject.Find("Background").GetComponent<Animator>();
+        AnimatorBack.Play("Start");
     }
 
     public void CalcularFn(string operation)
@@ -97,24 +108,108 @@ public class Calculator : MonoBehaviour
         if (operation == "sum")
         {
             finalAnswer = primeValue + secondValue;
+            VarSign = "sum";
         }
 
         if (operation == "reduce")
         {
             finalAnswer = primeValue - secondValue;
-        }
-
-        if (operation == "multiply")
-        {
-            finalAnswer = primeValue * secondValue;
+            VarSign = "reduce";
         }
 
         if (operation == "divide")
         {
             finalAnswer = primeValue / secondValue;
+            Debug.Log("Division: " + finalAnswer);
+            VarSign = "divide";
         }
 
+        if (operation == "multiply")
+        {
+            finalAnswer = primeValue * secondValue;
+            VarSign = "multiply";
+        }
 
-        Debug.Log("1Value: " + primeValue + " 2value: " + secondValue + "Answer is : "+ finalAnswer);
+        Debug.Log("1Value: " + primeValue + " 2value: " + secondValue + "Answer is : " + finalAnswer); // For pressing Letter S,R,M and D
+
+        PrimeDigit.text = primeValue.ToString();
+        SecondDigit.text = secondValue.ToString();
+
+        if (VarSign == "sum")
+        {
+            Sign.text = "+";
+        }
+
+        if (VarSign == "reduce")
+        {
+            Sign.text = "-";
+        }
+
+        if (VarSign == "divide")
+        {
+            Sign.text = "÷";
+        }
+
+        if (VarSign == "multiply")
+        {
+            Sign.text = "×";
+        }
+
+        // PRIMER ALTERNATIVE
+        tempValue = Random.Range(2, 20);
+        while (tempValue == finalAnswer)
+        {
+            tempValue = Random.Range(2, 20);
+        }
+        Alternative1 = tempValue;
+
+        // SECOND ALTERNATIVE
+        tempValue = Random.Range(2, 20);
+        while ((tempValue == finalAnswer) || (tempValue == Alternative1))
+        {
+            tempValue = Random.Range(2, 20);
+        }
+        Alternative2 = tempValue;
+
+        Debug.Log("Alternatives: " + Alternative1 + " - " + Alternative2 + " - " + finalAnswer);
+
+
+        tempValue = Random.Range(1, 7);
+        if (tempValue == 1)
+        {
+            Alt_1.text = finalAnswer.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = Alternative2.ToString();
+            Debug.Log("tempValue1");
+        }
+        if (tempValue == 2)
+        {
+            Alt_1.text = finalAnswer.ToString(); Alt_2.text = Alternative2.ToString(); Alt_3.text = Alternative1.ToString();
+            Debug.Log("tempValue2");
+        }
+        if (tempValue == 3)
+        {
+            Alt_1.text = Alternative1.ToString(); Alt_2.text = finalAnswer.ToString(); Alt_3.text = Alternative2.ToString();
+            Debug.Log("tempValue3");
+        }
+        if (tempValue == 4)
+        {
+            Alt_1.text = Alternative1.ToString(); Alt_2.text = Alternative2.ToString(); Alt_3.text = finalAnswer.ToString();
+            Debug.Log("tempValue4");
+        }
+        if (tempValue == 5)
+        {
+            Alt_1.text = Alternative2.ToString(); Alt_2.text = finalAnswer.ToString(); Alt_3.text = Alternative1.ToString();
+            Debug.Log("tempValue5");
+        }
+        if (tempValue == 6)
+        {
+            Alt_1.text = Alternative2.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = finalAnswer.ToString();
+            Debug.Log("tempValue6");
+        }
+        if (tempValue == 7)
+        {
+            Alt_1.text = Alternative2.ToString(); Alt_2.text = Alternative1.ToString(); Alt_3.text = finalAnswer.ToString();
+            Debug.Log("tempValue7");
+        }
+
     }
 }
